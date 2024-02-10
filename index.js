b606f9e346ae05186ef29683c5b76eab2a614634
Renderthegreat <renderthegreat.business@gmail.com>
2024-02-10 04:38:04 +0000

    Fix!

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
+let buffer = {}
 class Lab {
   constructor(options) {
     this.options = options;
   }
-  build() {
-
-  }
+  build() {}
 }
 function uProgress(progress) {
-    
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write(`Progress: ${progress}%`);
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`Progress: ${progress}%`);
 }
-
-
 
 class Ray {
   constructor(origin, direction) {
@@ -26,7 +22,7 @@ class Ray {
     return {
       x: this.origin.x + t * this.direction.x,
       y: this.origin.y + t * this.direction.y,
-      z: this.origin.z + t * this.direction.z
+      z: this.origin.z + t * this.direction.z,
     };
   }
 }
@@ -42,15 +38,16 @@ class Sphere {
     const oc = {
       x: ray.origin.x - this.center.x,
       y: ray.origin.y - this.center.y,
-      z: ray.origin.z - this.center.z
+      z: ray.origin.z - this.center.z,
     };
-    const a = ray.direction.x * ray.direction.x +
+    const a =
+      ray.direction.x * ray.direction.x +
       ray.direction.y * ray.direction.y +
       ray.direction.z * ray.direction.z;
-    const half_b = oc.x * ray.direction.x +
-      oc.y * ray.direction.y +
-      oc.z * ray.direction.z;
-    const c = oc.x * oc.x + oc.y * oc.y + oc.z * oc.z - this.radius * this.radius;
+    const half_b =
+      oc.x * ray.direction.x + oc.y * ray.direction.y + oc.z * ray.direction.z;
+    const c =
+      oc.x * oc.x + oc.y * oc.y + oc.z * oc.z - this.radius * this.radius;
     const discriminant = half_b * half_b - a * c;
     if (discriminant < 0) {
       return -1;
@@ -62,11 +59,14 @@ class Sphere {
 
 class Render {
   constructor(options) {
+    buffer.vec3 = this.vec3
     this.materials = options.materials || {};
-    this.defaultMaterial = options.defaultMaterial || { color: { r: 0.5, g: 0.5, b: 0.5 } };
+    buffer.materials = this.materials;
+    this.defaultMaterial = options.defaultMaterial || {
+      color: { r: 0.5, g: 0.5, b: 0.5 },
+    };
     this.shaders = options.shaders || {};
     this.defaultShader = this.shaders.default;
-    
   }
 
   vec3(x, y, z) {
@@ -102,13 +102,17 @@ class Render {
     }
     if (hitObject) {
       const shader = this.shaders[hitObject.shader] || this.defaultShader;
-      return shader(hitObject, ray);
+      let shard = shader(hitObject, ray);
+      shard.x = shard.r
+      shard.y = shard.g
+      shard.z = shard.b
+      return shard
     }
     const unitDirection = this.unitVector(ray.direction);
     const t = 0.5 * (unitDirection.y + 1.0);
     return this.add(
       this.scale(this.vec3(1.0, 1.0, 1.0), 1.0 - t),
-      this.scale(this.vec3(0.5, 0.7, 1.0), t)
+      this.scale(this.vec3(0.5, 0.7, 1.0), t),
     );
   }
 
@@ -117,20 +121,20 @@ class Render {
     const lowerLeftCorner = this.vec3(
       -viewportWidth / 2,
       -viewportHeight / 2,
-      -focalLength
+      -focalLength,
     );
     const horizontal = this.vec3(viewportWidth, 0, 0);
     const vertical = this.vec3(0, viewportHeight, 0);
     const origin = this.vec3(0, 0, 0);
-    let x = 0
-    let c = 0
-    const a = ( height - 1 ) * ( width - 1)
-    let e = 1
-    let xo = 0
-    while(a / e >= 100){
-      e = e * 10
+    let x = 0;
+    let c = 0;
+    const a = (height - 1) * (width - 1);
+    let e = 1;
+    let xo = 0;
+    while (a / e >= 100) {
+      e = e * 10;
     }
-    e *= 1000
+    e *= 1000;
     for (let j = height - 1; j >= 0; j--) {
       for (let i = 0; i < width; i++) {
         c++;
@@ -138,24 +142,26 @@ class Render {
         const v = j / (height - 1);
         const direction = this.add(
           this.add(lowerLeftCorner, this.scale(horizontal, u)),
-          this.scale(vertical, v)
+          this.scale(vertical, v),
         );
         const ray = new Ray(origin, direction);
         const color = this.rayColor(ray, objects);
+        
         const ir = Math.floor(255.999 * color.x);
         const ig = Math.floor(255.999 * color.y);
         const ib = Math.floor(255.999 * color.z);
         data += ir + " " + ig + " " + ib + "\n";
-        x = ((a/100)*c)/e/2.3
-        x = Math.round(x)
-        if(xo!=x){
-          uProgress(x)
-          xo = x
+        
+        x = ((a / 100) * c) / e / 2.3;
+        x = Math.round(x);
+        if (xo != x) {
+          uProgress(x);
+          xo = x;
         }
       }
       //console.log(j)
     }
-    
+
     return data;
   }
 }
@@ -163,18 +169,26 @@ class Render {
 const renderer = new Render({
   materials: {
     red: { color: { r: 1, g: 0, b: 0 } },
-    blue: { color: { r: 0, g: 0, b: 1 } }
+    blue: { color: { r: 0, g: 0, b: 1 } },
   },
   defaultMaterial: { color: { r: 0.5, g: 0.5, b: 0.5 } },
   shaders: {
     default: function (object, ray) {
-      return object.color;
-    }  }
+      let c = object.color;
+      
+      if(buffer.materials[c].color){
+        return buffer.materials[c].color;
+      }
+      else{
+        return c;
+      }
+    },
+  },
 });
 
 const objects = [
-  new Sphere({ x: 0, y: 0, z: -1 }, 0.5, 'red', 'default'),
-  new Sphere({ x: 0, y: -100.5, z: -1 }, 100, 'blue', 'default')
+  new Sphere({ x: 0, y: 0, z: -1 }, 0.5, "red", "default"),
+  new Sphere({ x: 0, y: -100.5, z: -1 }, 100, "blue", "default"),
 ];
 
 const width = 800;
@@ -183,6 +197,18 @@ const viewportWidth = 2.0;
 const viewportHeight = (height / width) * viewportWidth;
 const focalLength = 1.0;
 
-const imageData = renderer.render(objects, width, height, viewportWidth, viewportHeight, focalLength);
+const imageData = renderer.render(
+  objects,
+  width,
+  height,
+  viewportWidth,
+  viewportHeight,
+  focalLength,
+);
 
-fs.writeFileSync('output.ppm', imageData)
+fs.writeFileSync("output.ppm", imageData);
+if(imageData.includes("NaN")){
+  console.log("\nError: NaN");
+  let dumbErrorArea = imageData.split("NaN")
+    console.log(dumbErrorArea[1]);
+}
